f3748eaf912aa52febabc9c64839c662dc874769
Toqu3 <luiguibalarezo@gmail.com>
2015-11-24 18:48:34 -0500

    Segundo Commit modificando prueba anterior PhpStorm and Git

---
+++ protected/views/_email/curso_suscripcion.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8"/>
+</head>
+<body>
+    <p>
+        <strong><i>Nombre de contacto: </i></strong>
+        <span><?php echo $name ?></span>
+    </p>
+    <p>
+        <strong><i>Apellidos: </i></strong>
+        <span><?php echo $organization ?></span>
+    </p>
+    <p>
+        <strong><i>Correo: </i></strong>
+        <span><?php echo $email ?></span>
+    </p>
+    <p>
+        <strong><i>Celular: </i></strong>
+        <span><?php echo $subject ?></span>
+    </p>
+    <p>
+        <strong><i>Consultas: </i></strong>
+        <p><?php echo $msg ?></p>
+    </p>
+</body>
+</html>
